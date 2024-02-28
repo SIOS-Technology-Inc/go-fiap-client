6a1434473c88d2876e2b2f41ff5da600f8e857e9
ChinaSasaki <c-sasaki@sios.com>
2024-02-28 09:05:25 +0000

    複数存在したgo.modファイルを1つに整理

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"github.com/SIOS-Technology-Inc/go-fiap-client/pkg/fiap"
-)
+import "github.com/SIOS-Technology-Inc/go-fiap-client/pkg/fiap"
 
 func main() {
 	// この関数の呼び出しは、fiapservice.go の動作テストのために作成したものです。後で削除します。
--- pkg/fiap/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/SIOS-Technology-Inc/go-fiap-client/pkg/fiap
-
-go 1.21.7
