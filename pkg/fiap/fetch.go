a54cb967888cbdf6f647488bfc35b613a4de9fd4
ChinaSasaki <c-sasaki@sios.com>
2024-02-29 07:30:41 +0000

    動作チェックのために作成した関数を削除

--- pkg/fiap/fetch.go
+++ pkg/fiap/fetch.go
@@ -7,59 +7,11 @@ import (
 	"time"
 	"regexp"
 
-	"github.com/google/uuid"
 	"github.com/globusdigital/soap"
 
 	"github.com/SIOS-Technology-Inc/go-fiap-client/pkg/fiap/model"
 )
 
-// TODO この関数は、fiapservice.go の動作テストのために作成したものです。後で削除します。
-func TestGoWsdl() {
-	client := soap.NewClient("http://iot.info.nara-k.ac.jp/axis2/services/FIAPStorage",nil)
-	service := NewFIAPServiceSoap(client)
-
-	var uuidObj uuid.UUID
-	uuidObj, _ = uuid.NewRandom()
-
-	var storage QueryType = QueryTypeStorage
-	var val PositiveInteger = 1000
-	var attrTime AttrNameType = AttrNameTypeTime
-	var selectType SelectType = SelectTypeMaximum
-
-	header := &Header{
-		Query: &Query{
-			Id:             Uuidp(uuidObj),
-			AcceptableSize: &val,
-			Type:           &storage,
-			Key: []*Key{
-				{
-					Id:       "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
-					AttrName: &attrTime,
-					Select:   &selectType,
-				},
-			},
-		},
-	}
-
-	queryRQ := &QueryRQ{
-		Transport: &Transport{
-			Header: header,
-		},
-	}
-
-	res, err := service.Query(queryRQ)
-	if err != nil {
-		log.Fatalf("could't get point data: %v", err)
-	}
-	log.Println(*res)
-	log.Printf("%#v\n", *res)
-
-	xmlBytes, err := xml.MarshalIndent(res, "", "  ")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(xmlBytes))
-}
 
 
 func Fetch(connectionURL string, keys []model.UserInputKey, option *model.FetchOption) (pointSets map[string]([]model.ProcessedPointSet), points map[string]([]model.ProcessedPoint), err error) {
