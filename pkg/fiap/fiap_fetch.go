1ddc8f5b6d05921d39d1449383350fdff3c60aea
ChinaSasaki <c-sasaki@sios.com>
2024-03-18 02:31:17 +0000

    関数の分割方法を変更

--- pkg/fiap/fiap_fetch.go
+++ pkg/fiap/fiap_fetch.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"regexp"
+	"github.com/google/uuid"
 
 	"github.com/SIOS-Technology-Inc/go-fiap-client/pkg/fiap/model"
 	"github.com/SIOS-Technology-Inc/go-fiap-client/pkg/fiap/tools"
@@ -58,4 +59,24 @@ func fiapFetch(connectionURL string, keys []model.UserInputKey, option *model.Fe
 
 	// エラーがなければ結果を返す
 	return httpResponse, resBody, nil
+}
+
+func CreateQueryRQ (val model.PositiveInteger, option *model.FetchOnceOption, keys []model.UserInputKey) *model.QueryRQ {
+	var uuidObj uuid.UUID
+	uuidObj, _ = uuid.NewRandom()
+	
+	queryRQ := &model.QueryRQ{
+		Transport: &model.Transport{
+			Header: &model.Header{
+				Query: &model.Query{
+					Id: tools.GoogleUuidToUuidp(uuidObj),
+					AcceptableSize: tools.AcceptableSizep(val),
+					Type: tools.QueryTypep(model.QueryTypeStorage),
+					Cursor: tools.CursorStrpToUuidp(option.Cursor),
+					Key: tools.UserInputKeysToKeysp(keys),
+				},
+			},
+		},
+	}
+	return queryRQ
 }
