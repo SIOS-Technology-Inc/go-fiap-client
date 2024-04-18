0d1362293c0b9df56b86fb1639ee7cb2040e8714
ChinaSasaki <c-sasaki@sios.com>
2024-04-18 06:07:58 +0000

    fiap_fetch.goのテストを作成

--- pkg/fiap/fiap_fetch_test.go
+++ pkg/fiap/fiap_fetch_test.go
@@ -1,15 +1,22 @@
 package fiap
 
 import (
+	"encoding/xml"
+	"errors"
+	"io"
+	"net/http"
+	"strings"
 	"testing"
 	"time"
-	"errors"
+	"fmt"
+	"regexp"
 
 	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/SIOS-Technology-Inc/go-fiap-client/pkg/fiap/model"
 	"github.com/SIOS-Technology-Inc/go-fiap-client/pkg/fiap/testutil"
+	"github.com/SIOS-Technology-Inc/go-fiap-client/pkg/fiap/tools"
 )
 
 func TestFiapFetchResponseOnlyPoint(t *testing.T) {
@@ -358,4 +365,496 @@ func TestRequestFailure(t *testing.T) {
 	assert.Contains(t, err.Error(), "client.Call error")
 	assert.Nil(t, httpResponse)
 	assert.Nil(t, QueryRS)
+}
+
+
+type Envelope struct {
+	XMLName xml.Name `xml:"Envelope"`
+	Body    struct {
+		QueryRQ *model.QueryRQ `xml:"queryRQ"`
+	} `xml:"Body"`
+}
+
+func TestFiapFetchRequestOptionAndKeys(t *testing.T) {
+	// テストケースを定義
+	testCases := []struct {
+		name string
+		option *model.FetchOnceOption
+		keys []model.UserInputKey
+		expectedRequestOption *model.FetchOnceOption
+		expectedRequestKeys []model.Key
+	}{
+		{
+				name: "when all inputs are valid",
+				option: &model.FetchOnceOption{
+					AcceptableSize: 1000,
+					Cursor: "9c383255-d5ae-4e86-9ce6-b72800ee65f7",
+				},
+				keys: []model.UserInputKey{
+						{
+							ID: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+							Eq: testutil.TimeToTimep(time.Date(2021, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+							Neq: testutil.TimeToTimep(time.Date(2021, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+							Lt: testutil.TimeToTimep(time.Date(2021, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+							Gt: testutil.TimeToTimep(time.Date(2021, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+							Lteq: testutil.TimeToTimep(time.Date(2021, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+							Gteq: testutil.TimeToTimep(time.Date(2021, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+							MinMaxIndicator: model.SelectTypeMaximum,
+						},
+						{
+							ID: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+						},
+				},
+				expectedRequestOption: &model.FetchOnceOption{
+					AcceptableSize: 1000,
+					Cursor: "9c383255-d5ae-4e86-9ce6-b72800ee65f7",
+				},
+				expectedRequestKeys: []model.Key{
+					{
+						Id: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+						Eq: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+						Neq: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+						Lt: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+						Gt: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+						Lteq: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+						Gteq: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+						Select: "maximum",
+					},
+					{
+						Id: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+					},
+				},
+		},
+		{
+			name: "when option.AcceptableSize is set but option.Cursor is not set",
+			keys: []model.UserInputKey{
+					{
+						ID: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+					},
+			},
+			option: &model.FetchOnceOption{
+					AcceptableSize: 1000,
+			},
+			expectedRequestOption: &model.FetchOnceOption{
+				AcceptableSize: 1000,
+			},
+			expectedRequestKeys: []model.Key{
+				{
+					Id: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+				},
+			},
+		},
+		{
+			name: "when option.Cursor is set but option.AcceptableSize is not set",
+			keys: []model.UserInputKey{
+					{
+						ID: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+					},
+			},
+			option: &model.FetchOnceOption{
+				Cursor: "9c383255-d5ae-4e86-9ce6-b72800ee65f7",
+			},
+			expectedRequestOption: &model.FetchOnceOption{
+				Cursor: "9c383255-d5ae-4e86-9ce6-b72800ee65f7",
+			},
+			expectedRequestKeys: []model.Key{
+				{
+					Id: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+				},
+			},
+		},
+		{
+			name: "when option.AcceptableSize and option.Cursor are set",
+			keys: []model.UserInputKey{
+					{
+						ID: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+					},
+			},
+			option: &model.FetchOnceOption{
+				AcceptableSize: 1000,
+				Cursor: "9c383255-d5ae-4e86-9ce6-b72800ee65f7",
+			},
+			expectedRequestOption: &model.FetchOnceOption{
+				AcceptableSize: 1000,
+				Cursor: "9c383255-d5ae-4e86-9ce6-b72800ee65f7",
+			},
+			expectedRequestKeys: []model.Key{
+				{
+					Id: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+				},
+			},
+		},
+		{
+			name : "when UserInputKey.MinMaxIndicator is SelectTypeMaximum",
+			keys: []model.UserInputKey{
+				{
+					ID: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+					MinMaxIndicator: model.SelectTypeMaximum,
+				},
+			},
+			option: nil,
+			expectedRequestOption: &model.FetchOnceOption{},
+			expectedRequestKeys: []model.Key{
+				{
+					Id: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+					Select: "maximum",
+				},
+			},
+		},
+		{
+			name : "when UserInputKey.MinMaxIndicator is SelectTypeMinimum",
+			keys: []model.UserInputKey{
+				{
+					ID: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+					MinMaxIndicator: model.SelectTypeMinimum,
+				},
+			},
+			option: nil,
+			expectedRequestOption: &model.FetchOnceOption{},
+			expectedRequestKeys: []model.Key{
+				{
+					Id: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+					Select: "minimum",
+				},
+			},
+		},
+		{
+			name : "when keys contain one UserInputKey",
+			keys: []model.UserInputKey{
+				{
+					ID: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+					Eq: testutil.TimeToTimep(time.Date(2021, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+					Neq: testutil.TimeToTimep(time.Date(2021, 1, 2, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+					Lt: testutil.TimeToTimep(time.Date(2021, 1, 3, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+					Gt: testutil.TimeToTimep(time.Date(2021, 1, 4, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+					Lteq: testutil.TimeToTimep(time.Date(2021, 1, 5, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+					Gteq: testutil.TimeToTimep(time.Date(2021, 1, 6, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+					MinMaxIndicator: model.SelectTypeMaximum,
+				},
+			},
+			option: nil,
+			expectedRequestOption: &model.FetchOnceOption{},
+			expectedRequestKeys: []model.Key{
+				{
+					Id: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+					Eq: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+					Neq: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 2, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+					Lt: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 3, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+					Gt: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 4, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+					Lteq: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 5, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+					Gteq: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 6, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+					Select: "maximum",
+				},
+			},
+		},
+		{
+			name : "when keys contain two UserInputKey",
+			keys: []model.UserInputKey{
+				{
+					ID: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+					Eq: testutil.TimeToTimep(time.Date(2021, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+					Neq: testutil.TimeToTimep(time.Date(2021, 1, 2, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+					Lt: testutil.TimeToTimep(time.Date(2021, 1, 3, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+					Gt: testutil.TimeToTimep(time.Date(2021, 1, 4, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+					Lteq: testutil.TimeToTimep(time.Date(2021, 1, 5, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+					Gteq: testutil.TimeToTimep(time.Date(2021, 1, 6, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+					MinMaxIndicator: model.SelectTypeMaximum,
+				},
+				{
+					ID: "http://kurimoto/nukaya/vaisala/B-2/Humidity_TD",
+					Eq: testutil.TimeToTimep(time.Date(2021, 1, 6, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+					Neq: testutil.TimeToTimep(time.Date(2021, 1, 5, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+					Lt: testutil.TimeToTimep(time.Date(2021, 1, 4, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+					Gt: testutil.TimeToTimep(time.Date(2021, 1, 3, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+					Lteq: testutil.TimeToTimep(time.Date(2021, 1, 2, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+					Gteq: testutil.TimeToTimep(time.Date(2021, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60))),
+					MinMaxIndicator: model.SelectTypeMinimum,
+				},
+			},
+			option: nil,
+			expectedRequestOption: &model.FetchOnceOption{},
+			expectedRequestKeys: []model.Key{
+				{
+					Id: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+					Eq: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+					Neq: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 2, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+					Lt: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 3, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+					Gt: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 4, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+					Lteq: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 5, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+					Gteq: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 6, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+					Select: "maximum",
+				},
+				{
+					Id: "http://kurimoto/nukaya/vaisala/B-2/Humidity_TD",
+					Eq: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 6, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+					Neq: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 5, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+					Lt: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 4, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+					Gt: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 3, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+					Lteq: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 2, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+					Gteq: tools.TimeToString(testutil.TimeToTimep(time.Date(2021, 1, 1, 0, 0, 0, 0, time.FixedZone("Asia/Tokyo", 9*60*60)))),
+					Select: "minimum",
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// httpmockの有効化
+			httpmock.Activate()
+			defer httpmock.DeactivateAndReset()
+
+			// Matcher function
+			matcher := httpmock.NewMatcher("",func(req *http.Request) bool {
+				envelope := &Envelope{}
+				err := xml.NewDecoder(req.Body).Decode(envelope)
+				if err != nil {
+						return false
+				}
+				if envelope.Body.QueryRQ.Transport.Header.Query.AcceptableSize == tc.expectedRequestOption.AcceptableSize &&
+					envelope.Body.QueryRQ.Transport.Header.Query.Cursor == tc.expectedRequestOption.Cursor &&
+					len(envelope.Body.QueryRQ.Transport.Header.Query.Key) == len(tc.expectedRequestKeys) {
+						for i := range envelope.Body.QueryRQ.Transport.Header.Query.Key {
+									if envelope.Body.QueryRQ.Transport.Header.Query.Key[i].Id == tc.expectedRequestKeys[i].Id && 
+								 envelope.Body.QueryRQ.Transport.Header.Query.Key[i].AttrName == "time" &&
+								 envelope.Body.QueryRQ.Transport.Header.Query.Key[i].Eq == tc.expectedRequestKeys[i].Eq &&
+								 envelope.Body.QueryRQ.Transport.Header.Query.Key[i].Neq == tc.expectedRequestKeys[i].Neq &&
+								 envelope.Body.QueryRQ.Transport.Header.Query.Key[i].Lt == tc.expectedRequestKeys[i].Lt &&
+								 envelope.Body.QueryRQ.Transport.Header.Query.Key[i].Gt == tc.expectedRequestKeys[i].Gt &&
+								 envelope.Body.QueryRQ.Transport.Header.Query.Key[i].Lteq == tc.expectedRequestKeys[i].Lteq &&
+								 envelope.Body.QueryRQ.Transport.Header.Query.Key[i].Gteq == tc.expectedRequestKeys[i].Gteq &&
+								 envelope.Body.QueryRQ.Transport.Header.Query.Key[i].Select == tc.expectedRequestKeys[i].Select {
+								return true
+							}
+						}
+					return false
+				}
+				return false
+			})
+
+			// Responder function
+			responder := testutil.CustomBodyResponder("")
+
+			// 下記URLにPOSTし、かつ特定のrequest bodyを送信したときの挙動を定義
+			httpmock.RegisterMatcherResponder("POST", "http://iot.info.nara-k.ac.jp/axis2/services/FIAPStorage",
+				matcher,
+				responder,
+			)
+
+			// テスト対象の関数を実行
+			httpResponse, _, err := fiapFetch(
+				"http://iot.info.nara-k.ac.jp/axis2/services/FIAPStorage",
+				tc.keys,
+				tc.option,
+			)
+			if err != nil {
+				t.Fatalf("fiapFetch failed: %v", err)
+			}
+			if httpResponse == nil {
+				t.Fatal("httpResponse is nil")
+			}
+			assert.Equal(t, 200, httpResponse.StatusCode)
+		})
+	}
+}
+
+func TestFiapFetchRequestWithoutSpecificString(t *testing.T) {
+	testCases := []struct {
+			name string
+			keys []model.UserInputKey
+			option *model.FetchOnceOption
+			notExpectedString []string  // 含まれてほしくない文字列を追加
+	}{
+			{
+					name: "when option is nil",
+					keys: []model.UserInputKey{
+							{
+								ID: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+							},
+					},
+					option: nil,
+					notExpectedString: []string{"acceptableSize", "cursor"},
+			},
+			{
+					name: "when option.AcceptableSize is set but option.Cursor is not set",
+					keys: []model.UserInputKey{
+							{
+								ID: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+							},
+					},
+					option: &model.FetchOnceOption{
+							AcceptableSize: 1000,
+					},
+					notExpectedString: []string{"cursor"},
+			},
+			{
+				name: "when option.Cursor is set but option.AcceptableSize is not set",
+				keys: []model.UserInputKey{
+						{
+							ID: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+						},
+				},
+				option: &model.FetchOnceOption{
+					Cursor: "9c383255-d5ae-4e86-9ce6-b72800ee65f7",
+				},
+				notExpectedString: []string{"acceptableSize"},
+		},
+		{
+			name: "when UserInputKey.MinMaxIndicator is SelectTypeNone",
+			keys: []model.UserInputKey{
+				{
+					ID: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+					MinMaxIndicator: model.SelectTypeNone,
+				},
+			},
+			option: nil,
+			notExpectedString: []string{"select"},
+		},
+		{
+			name: "when UserInputKey.MinMaxIndicator is not set",
+			keys: []model.UserInputKey{
+				{
+					ID: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD",
+				},
+			},
+			option: nil,
+			notExpectedString: []string{"select"},
+		},
+	}
+
+	for _, tc := range testCases {
+			t.Run(tc.name, func(t *testing.T) {
+					// httpmockの有効化
+					httpmock.Activate()
+					defer httpmock.DeactivateAndReset()
+
+					// Matcher function
+					matcher := httpmock.NewMatcher("", func(req *http.Request) bool {
+						bodyBytes, err := io.ReadAll(req.Body)
+						if err != nil {
+							return false
+						}
+						bodyString := string(bodyBytes)
+						fmt.Println(bodyString)
+
+
+						for _, tag := range tc.notExpectedString {
+							if strings.Contains(bodyString, tag) {
+								return false
+							}
+						}
+						return true
+					})
+
+					// Responder function
+					responder := testutil.CustomBodyResponder("")
+
+					// 下記URLにPOSTし、かつ特定のrequest bodyを送信したときの挙動を定義
+					httpmock.RegisterMatcherResponder("POST", "http://iot.info.nara-k.ac.jp/axis2/services/FIAPStorage",
+							matcher,
+							responder,
+					)
+
+					// テスト対象の関数を実行
+					httpResponse, _, err := fiapFetch(
+							"http://iot.info.nara-k.ac.jp/axis2/services/FIAPStorage",
+							tc.keys,
+							tc.option,
+					)
+					if err != nil {
+							t.Fatalf("fiapFetch failed: %v", err)
+					}
+					if httpResponse == nil {
+							t.Fatal("httpResponse is nil")
+					}
+					assert.Equal(t, 200, httpResponse.StatusCode)
+			})
+	}
+}
+
+func TestFiapFetchRequestQueryType(t *testing.T) {
+	// httpmockの有効化
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	// Matcher function
+	matcher := httpmock.NewMatcher("",func(req *http.Request) bool {
+		envelope := &Envelope{}
+		err := xml.NewDecoder(req.Body).Decode(envelope)
+		if err != nil {
+				return false
+		}
+		if envelope.Body.QueryRQ.Transport.Header.Query.Type == "storage" {	
+			return true
+		}
+		return false
+	})
+
+	// Responder function
+	responder := testutil.CustomBodyResponder("")
+
+	// 下記URLにPOSTし、かつ特定のrequest bodyを送信したときの挙動を定義
+	httpmock.RegisterMatcherResponder("POST", "http://iot.info.nara-k.ac.jp/axis2/services/FIAPStorage",
+		matcher,
+		responder,
+	)
+
+	// テスト対象の関数を実行
+	httpResponse, _, err := fiapFetch(
+		"http://iot.info.nara-k.ac.jp/axis2/services/FIAPStorage",
+		[]model.UserInputKey{
+				{ID: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD"},
+		},
+		nil,
+)
+if err != nil {
+	t.Fatalf("fiapFetch failed: %v", err)
+}
+if httpResponse == nil {
+	t.Fatal("httpResponse is nil")
+}
+	assert.Equal(t, 200, httpResponse.StatusCode)
+}
+
+func TestFiapFetchRequestUuid(t *testing.T) {
+	// httpmockの有効化
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	// Matcher function
+	matcher := httpmock.NewMatcher("",func(req *http.Request) bool {
+		envelope := &Envelope{}
+		err := xml.NewDecoder(req.Body).Decode(envelope)
+		if err != nil {
+				return false
+		}
+
+		uuidRegex := regexp.MustCompile(`^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$`)
+		return uuidRegex.MatchString(envelope.Body.QueryRQ.Transport.Header.Query.Id)
+	})
+
+	// Responder function
+	responder := testutil.CustomBodyResponder("")
+
+	// 下記URLにPOSTし、かつ特定のrequest bodyを送信したときの挙動を定義
+	httpmock.RegisterMatcherResponder("POST", "http://iot.info.nara-k.ac.jp/axis2/services/FIAPStorage",
+		matcher,
+		responder,
+	)
+
+	// テスト対象の関数を実行
+	httpResponse, _, err := fiapFetch(
+		"http://iot.info.nara-k.ac.jp/axis2/services/FIAPStorage",
+		[]model.UserInputKey{
+				{ID: "http://kurimoto/nukaya/vaisala/B-2/Temperature_TD"},
+		},
+		nil,
+)
+if err != nil {
+	t.Fatalf("fiapFetch failed: %v", err)
+}
+if httpResponse == nil {
+	t.Fatal("httpResponse is nil")
+}
+	assert.Equal(t, 200, httpResponse.StatusCode)
 }
